af536861cfc3e45a7437b30c2fb3f63d416d2b7b
Vitor Brenguere <vitorbrenguere@gmail.com>
2026-02-25 20:16:05 -0300

    Criado a Controller

---
+++ src/main/java/com/cadastrodeninjas/Controller.java
@@ -0,0 +1,16 @@
+package com.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/bemvindo")
+    public String bemVindo(){
+        return "Essa é a minha primeira mensagem nessa rota de SPRINGBOOT!";
+    }
+
+}
